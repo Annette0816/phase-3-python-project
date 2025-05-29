c7826d40324204f8ff5f605ef97c4ebc4ec7feba
Annette <annette.ngugi@student.moringaschool.com>
2025-05-29 15:39:28 +0300

    updated album.py

--- lib/models/album.py
+++ lib/models/album.py
@@ -3,44 +3,62 @@ from models.__init__ import CURSOR, CONN
 class Album:
     all = {}
 
-    def __init__(self, title, genre, id=None):
+    def __init__(self, title, release_date, artist, id=None):
         self.id = id
         self.title = title
-        self.genre = genre
-       
+        self.release_date = release_date
+        self.artist = artist
 
     def __repr__(self):
-        return f"<Album {self.id}: {self.title} : {self.genre}>"
+        return f"<Album {self.id}: {self.title}  {self.release_date} by {self.artist.stage_name}>"
 
-    # @property
-    # def title(self):
-    #     return self._title
+    @property
+    def title(self):
+        return self._title
 
-    # @title.setter
-    # def name(self, value):
-    #     if isinstance(value, str) and len(value):
-    #         self._title = value
-    #     else:
-    #         raise ValueError("Title must be a non-empty string.")
+    @title.setter
+    def  title(self, value):
+        if isinstance(value, str) and len(value):
+            self._title = value
+        else:
+            raise ValueError("Title must be a non-empty string.")
 
-    # @property
-    # def genre(self):
-    #     return self._genre
-    # @genre.setter
-    # def genre(self, value):
-    #     if isinstance(value, str) and len(value):
-    #         self._genre = value
-    #     else:
-    #         raise ValueError("Genre must be a non-empty string.")
+    @property
+    def release_date(self):
+        return self._release_date
 
+    @release_date.setter
+    def release_date(self, value):
+        if isinstance(value, str) and len(value):
+            self._release_date = value
+        else:
+            raise ValueError("Release date must be a non-empty string.")
+
+    @property
+    def artist(self):
+        return self._artist
+
+    @artist.setter
+    def artist(self, value):
+        from models.artist import Artist
+        if isinstance(value, Artist):
+            self._artist = value
+        else:
+            raise ValueError("Artist must be an instance of Artist.")
    
+    def songs(self):
+     from lib.models.song import Song
+     return [song for song in Song.all.values() if song.album.id == self.id]
+
     @classmethod
     def create_table(cls):
         sql = """
             CREATE TABLE IF NOT EXISTS albums (
                 id INTEGER PRIMARY KEY,
                 title TEXT NOT NULL,
-                genre TEXT NOT NULL
+                release_date TEXT NOT NULL,
+                artist_id INTEGER,
+                FOREIGN KEY (artist_id) REFERENCES artists(id)
             );
         """
         CURSOR.execute(sql)
@@ -52,17 +70,20 @@ class Album:
         CONN.commit()
 
     def save(self):
-        sql = "INSERT INTO albums (title, genre) VALUES (?, ?)"
-        CURSOR.execute(sql, (self.title, self.genre))
-        CONN.commit()
-        self.id = CURSOR.lastrowid
-        type(self).all[self.id] = self
+       sql = """
+        INSERT INTO albums (title, release_date, artist_id)
+        VALUES (?, ?, ?)
+    """
+       CURSOR.execute(sql, (self.title, self.release_date, self.artist.id))
+       CONN.commit()
+       self.id = CURSOR.lastrowid
+       type(self).all[self.id] = self
 
     @classmethod
-    def create(cls, title, genre):
-        album = cls(title, genre)
-        album.save()
-        return album
+    def create(cls, title, release_date, artist):
+     album = cls(title, release_date, artist)
+     album.save()
+     return album
 
     @classmethod
     def find_by_id(cls, id):
@@ -75,8 +96,8 @@ class Album:
         return cls.instance_from_db(row) if row else None
 
     @classmethod
-    def find_by_genre(cls, genre):
-        row = CURSOR.execute("SELECT * FROM albums WHERE genre = ?", (genre,)).fetchone()
+    def find_by_release_date(cls, release_date):
+        row = CURSOR.execute("SELECT * FROM albums WHERE release_date = ?", (release_date,)).fetchone()
         return cls.instance_from_db(row) if row else None
     
     @classmethod
@@ -87,6 +108,6 @@ class Album:
     
     @classmethod
     def instance_from_db(cls, row):
-        return cls(row["title"], row["genre"], row["id"])
+        return cls(row["title"], row["release_date"], row["id"])
 
    
